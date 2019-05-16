ee01a432d0ade361b825c4bcdc63512dd0bca7dc
ProgTJ <jtprogenitor@gmail.com>
2019-05-16 18:14:53 -0400

    edits

--- Login/login.php
+++ Login/login.php
@@ -1,13 +1,15 @@
 <?php
    session_start();
    include '../Connection/db.inc.php';
+
+  
     
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     $filter = [
-        'username'     => $username,
-        'password'     => $password
+        'username' => $username,
+        'password' => $password
     ];
     
     $query = new MongoDB\Driver\Query($filter);
@@ -15,13 +17,14 @@
     try{
         
         $result = $manager->executeQuery('gyna.user',$query);
-        var_dump($result);
-        //$row = $result->toArray();
-        //$user = $row[0]->username;
-       // $_SESSION['username'] = $user;
-       // header("Location: ../Home/index.php");
+        //var_dump($result);
+        $row = $result->toArray();
+        $user = $row[0]->username;
+       $_SESSION['username'] = $user;
+       header("Location: ../Home/index.php");
 
     } catch(MongoDB\Driver\Exception\Exception $e){
         die("Error Encountered: ".$e);
     }
+
 ?>
