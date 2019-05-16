ec5b3e4dee3de13b6a6c4c691ae9a7666f926856
ProgTJ <jtprogenitor@gmail.com>
2019-05-16 18:48:39 -0400

    test2

--- Login/login.php
+++ Login/login.php
@@ -1,30 +1,38 @@
-<?php
-   session_start();
-   include '../Connection/db.inc.php';
-
-  
-    
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    $filter = [
-        'username' => $username,
-        'password' => $password
-    ];
-    
-    $query = new MongoDB\Driver\Query($filter);
-
-    try{
-        
-        $result = $manager->executeQuery('gyna.user',$query);
-        //var_dump($result);
-        $row = $result->toArray();
-        $user = $row[0]->username;
-       $_SESSION['username'] = $user;
-       header("Location: ../Home/index.php");
-
-    } catch(MongoDB\Driver\Exception\Exception $e){
-        die("Error Encountered: ".$e);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="loginstyle.css"  rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>Project Gyna Login</title>
+</head>
+<body>
+    <?php
+    if(isset($_POST['username'])) {
+        echo $_POST['username'];
     }
+    ?>
 
-?>
+        <div class="loginform">
+                <h1>Gyna</h1>
+                <hr>
+                    <form action="login_inc.php" method="POST" id="loginform">
+                        <div class="loginfield">
+                            <h3>Username</h3>
+                            <input type="text" placeholder="" name="username" id="input">
+                        </div>
+
+                        <div class="loginfield">
+                            <h3>Password</h3>
+                            <input type="password" placeholder="" name="password" id="input">
+                        </div>
+
+                        <button type="submit" class="submitbutton" value="submit">Login</button>
+                    </form>
+                    <p>Don't have an account? <a href="signup.html">signup</a> </p>
+        </div>
+        
+</body>
+</html>
